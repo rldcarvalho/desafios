f78c9802633031c8b58ae78809329d66353bc37f
Rafael <rldcarvalhoo@gmail.com>
2022-11-20 19:23:08 -0300

    Desafio Multiples of three or five

--- java/.idea/sonarlint/issuestore/index.pb
+++ java/.idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 \
 ,src/main/java/challenges/FirstFactorial.java,6\4\6479a330bb8db8b8ef052bffec8d9c2f1c7723de
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Z
+*src/main/java/challenges/FirstReverse.java,c\3\c38f700b8764a59be75fee6d6a6866224139bf09
---
+++ java/src/main/java/challenges/MultiplesOfThreeOrFive.java
@@ -0,0 +1,31 @@
+package challenges;
+
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+public class MultiplesOfThreeOrFive {
+
+    public static int solution(int number) {
+
+        int answer = 0;
+
+        for (int i = 1; i < number ; i++) {
+            if(i % 3 == 0 || i % 5 == 0){
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+
+}
