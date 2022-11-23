741b902219900ee1ef80a03fdf0d87377f9329ef
Rafael <rldcarvalhoo@gmail.com>
2022-11-22 22:33:27 -0300

    Desafio Spin Words

---
+++ java/src/main/java/challenges/SpinWords.java
@@ -0,0 +1,38 @@
+package challenges;
+
+/*
+Write a function that takes in a string of one or more words, and returns the same string,
+but with all five or more letter words reversed (Just like the name of this Kata).
+Strings passed in will consist of only letters and spaces. Spaces will be included only
+when more than one word is present.
+
+Examples:
+
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona test"
+ */
+
+public class SpinWords {
+
+    public String spinWords(String sentence) {
+
+        if (sentence.isBlank()) return sentence;
+
+        String[] words = sentence.split("\\s");
+
+        StringBuilder finalString = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() >= 5) {
+                StringBuilder sb = new StringBuilder(word);
+                sb.reverse();
+                finalString.append(sb).append(" ");
+            }else {
+                finalString.append(word).append(" ");
+            }
+        }
+
+        return finalString.toString().trim();
+    }
+}
