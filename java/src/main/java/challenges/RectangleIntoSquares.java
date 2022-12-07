519b008e4375ff4deb2c4c9e9980bd99b68bc32f
Rafael <rldcarvalhoo@gmail.com>
2022-12-06 22:32:00 -0300

    Adicionado descrição Rectangle Into Squares

--- java/src/main/java/challenges/RectangleIntoSquares.java
+++ java/src/main/java/challenges/RectangleIntoSquares.java
@@ -1,5 +1,22 @@
 package challenges;
 
+/*
+The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+
+*image* 5x3 rectangle with 3x3, 2x2, 1x1 and 1x1 squares inside it.
+
+Can you translate this drawing into an algorithm?
+You will be given two dimensions
+
+a positive integer length
+a positive integer width
+
+Examples in general form:
+
+  sqInRect(5, 3) should return [3, 2, 1, 1]
+  sqInRect(3, 5) should return [3, 2, 1, 1]
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
