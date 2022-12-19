838992ccec9cba1bfe2451c37fde47b1fb29119f
Rafael <rldcarvalhoo@gmail.com>
2022-12-19 18:49:42 -0300

    Desafio Who Likes It? Alternativa

--- java/src/main/java/challenges/WhoLikesIt.java
+++ java/src/main/java/challenges/WhoLikesIt.java
@@ -37,4 +37,15 @@ public class WhoLikesIt {
         }
     }
 
+    public static String whoLikesItBestPractices(String... names){
+
+        return switch (names.length) {
+            case 0 -> "no one likes this";
+            case 1 -> String.format("%s like this", names[0]);
+            case 2 -> String.format("%s and %s like this", names[0], names[1]);
+            case 3 -> String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default -> String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        };
+    }
+
 }
