ef573e02a94e133e9dd6be5232e5f87eb83fc061
Rafael <rldcarvalhoo@gmail.com>
2022-12-21 16:08:51 -0300

    Desafio Create Phone Number Alternativa

--- java/src/main/java/challenges/CreatePhoneNumber.java
+++ java/src/main/java/challenges/CreatePhoneNumber.java
@@ -11,4 +11,9 @@ public class CreatePhoneNumber {
         return strNumbers.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
     }
 
+    public static String createPhoneNumberBestPractices(int[] numbers) {
+
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", numbers[0], numbers[1], numbers[2], numbers[3],
+                numbers[4], numbers[5], numbers[6], numbers[7], numbers[8], numbers[9]);
+    }
 }
