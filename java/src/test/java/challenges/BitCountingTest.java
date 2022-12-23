f2c3e8ea6f6c3be83f78fd96bc84a072377b9275
Rafael <rldcarvalhoo@gmail.com>
2022-12-23 16:22:06 -0300

    Desafio Bit Counting

---
+++ java/src/test/java/challenges/BitCountingTest.java
@@ -0,0 +1,18 @@
+package challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitCountingTest {
+
+    @Test
+    void deveriaRetornarONumeroDeBitsDoValorInformado(){
+        assertEquals(5, BitCounting.countBits(1234));
+        assertEquals(1, BitCounting.countBits(4));
+        assertEquals(3, BitCounting.countBits(7));
+        assertEquals(2, BitCounting.countBits(9));
+        assertEquals(2, BitCounting.countBits(10));
+    }
+
+}
