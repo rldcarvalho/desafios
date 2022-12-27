d769aac7370a8ddf9bfdb6f5bb2238c4e0baaecd
Rafael <rldcarvalhoo@gmail.com>
2022-12-27 15:38:40 -0300

    Desafio Ten Min Walk

---
+++ java/src/test/java/challenges/TenMinWalkTest.java
@@ -0,0 +1,18 @@
+package challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TenMinWalkTest {
+
+    @Test
+    void deveriaRetornarTrueParaCaminhadaQueRetornaEmMenosDeDezMin(){
+
+        assertTrue(TenMinWalk.isValid(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'w'}));
+        assertFalse(TenMinWalk.isValid(new char[]{'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+    }
+
+}
